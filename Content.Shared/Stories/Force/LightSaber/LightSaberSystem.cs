a3a2b160537b2bcb6641775cf712528fdd723c31
Doublechest <twin01v@gmail.com>
2023-11-18 21:40:43 +0300

    Block pickup tethered LightSaber

--- Content.Shared/Stories/Force/LightSaber/LightSaberSystem.cs
+++ Content.Shared/Stories/Force/LightSaber/LightSaberSystem.cs
@@ -2,6 +2,8 @@ using Content.Shared.Inventory.Events;
 using Content.Shared.SpaceStories.Force.ForceSensitive;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Popups;
+using Content.Shared.Item;
+using Content.Shared.Weapons.Misc;
 
 namespace Content.Shared.SpaceStories.Force.LightSaber;
 public sealed class LightSaberSystem : EntitySystem
@@ -13,13 +15,20 @@ public sealed class LightSaberSystem : EntitySystem
         base.Initialize();
         SubscribeLocalEvent<LightSaberComponent, GotEquippedEvent>(OnEquipped);
         SubscribeLocalEvent<LightSaberComponent, UseInHandEvent>(OnUseInHand);
+        SubscribeLocalEvent<LightSaberComponent, GettingPickedUpAttemptEvent>(OnTryPickUp);
+    }
+
+    private void OnTryPickUp(EntityUid uid, LightSaberComponent component, GettingPickedUpAttemptEvent args)
+    {
+        if (component.LightSaberOwner != null && args.User != component.LightSaberOwner && HasComp<TetheredComponent>(uid))
+            args.Cancel();
     }
     private void OnUseInHand(EntityUid uid, LightSaberComponent comp, UseInHandEvent args)
     {
         if (comp.LightSaberOwner.Equals(args.User) || comp.LightSaberOwner == null)
             return;
 
-        _popup.PopupClient(Loc.GetString("Вам кажется, что вас меч кто-то трогает..."), comp.LightSaberOwner.Value, comp.LightSaberOwner.Value);
+        _popup.PopupClient(Loc.GetString("Вам кажется, что ваш меч кто-то трогает..."), comp.LightSaberOwner.Value, comp.LightSaberOwner.Value);
     }
     private void OnEquipped(EntityUid uid, LightSaberComponent comp, GotEquippedEvent args)
     {
