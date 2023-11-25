5cd1eaf1b74f44bee83ffc6d7d6a55fb11615aeb
Lokilife <lokilife0002@gmail.com>
2023-11-25 20:43:06 +0300

    Add kidans

---
+++ Content.Shared/Stories/Damage/SharedDamageModificatorSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Shared.Inventory;
+
+namespace Content.Shared.Damage;
+
+/// <summary>
+/// This handles logic relating to <see cref="DamageModificatorComponent"/>
+/// </summary>
+public abstract class SharedDamageModificatorSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<DamageModificatorComponent, InventoryRelayedEvent<DamageModifyEvent>>(OnDamageModify);
+    }
+
+    private void OnDamageModify(EntityUid uid, DamageModificatorComponent component, InventoryRelayedEvent<DamageModifyEvent> args)
+    {
+        Log.Debug("Modified damage");
+        args.Args.Damage = DamageSpecifier.ApplyModifierSet(args.Args.Damage, component.Modifiers);
+    }
+}
