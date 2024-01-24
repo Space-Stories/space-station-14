14bf66c9e470efe8ecbff19a6065b55f17d9e71d
MetalSage <retking099@gmail.com>
2024-01-24 16:07:37 +0400

    queue fix

--- Content.Client/Corvax/JoinQueue/QueueState.cs
+++ Content.Client/Corvax/JoinQueue/QueueState.cs
@@ -1,29 +1,27 @@
 ﻿using Content.Shared.Corvax.JoinQueue;
+using Robust.Client.Audio;
 using Robust.Client.Console;
 using Robust.Client.GameObjects;
 using Robust.Client.State;
 using Robust.Client.UserInterface;
-using Robust.Shared.Audio;
-using Robust.Shared.Audio.Systems;
 using Robust.Shared.Player;
 
 namespace Content.Client.Corvax.JoinQueue;
 
 public sealed class QueueState : State
 {
-    [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly IUserInterfaceManager _userInterfaceManager = default!;
     [Dependency] private readonly IClientConsoleHost _consoleHost = default!;
 
     private const string JoinSoundPath = "/Audio/Effects/voteding.ogg";
 
     private QueueGui? _gui;
-    
+
     protected override void Startup()
     {
         _gui = new QueueGui();
         _userInterfaceManager.StateRoot.AddChild(_gui);
-        
+
         _gui.QuitPressed += OnQuitPressed;
     }
 
@@ -37,14 +35,17 @@ public sealed class QueueState : State
 
     private void Ding()
     {
-        _audio.PlayGlobal(JoinSoundPath, Filter.Local(), false);
+        if (IoCManager.Resolve<IEntityManager>().TrySystem<AudioSystem>(out var audio))
+        {
+            audio.PlayGlobal(JoinSoundPath, Filter.Local(), false);
+        }
     }
-    
+
     public void OnQueueUpdate(MsgQueueUpdate msg)
     {
         _gui?.UpdateInfo(msg.Total, msg.Position);
     }
-    
+
     private void OnQuitPressed()
     {
         _consoleHost.ExecuteCommand("quit");
