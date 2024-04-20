295e0eded039dced00b1aece8b634fca66173629
Doublechest <twin01v@gmail.com>
2024-04-20 23:33:32 +0300

    fix

--- Content.Server/Stories/Partners/Managers/PartnersManager.cs
+++ Content.Server/Stories/Partners/Managers/PartnersManager.cs
@@ -23,11 +23,16 @@ public sealed class PartnersManager : IPartnersManager
     private ISawmill _sawmill = default!;
     public void SetAntagPicked(NetUserId userId)
     {
+        if (_db.FullState == System.Data.ConnectionState.Closed || _db.FullState == System.Data.ConnectionState.Broken)
+            return;
+
         using NpgsqlCommand cmd = new NpgsqlCommand($"""UPDATE partners SET "last_day_taking_antag" = {DateTime.Now.DayOfYear} WHERE partners.net_id = '{userId.UserId.ToString()}'""", _db);
         using NpgsqlDataReader reader = cmd.ExecuteReader();
     }
     public bool TryGetInfo(NetUserId userId, [NotNullWhen(true)] out DbSponsorInfo? sponsor)
     {
+        Init();
+
         sponsor = null;
 
         if (_db.FullState == System.Data.ConnectionState.Closed || _db.FullState == System.Data.ConnectionState.Broken)
