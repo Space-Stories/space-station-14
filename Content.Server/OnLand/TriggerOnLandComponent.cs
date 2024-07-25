bf4e72a0d1540328c3d918b00a1e268fe0a5ca2f
ReichDSD <antonyy228@mail.ru>
2024-07-25 17:55:11 +0700

    New server side component.

---
+++ Content.Server/OnLand/TriggerOnLandComponent.cs
@@ -0,0 +1,17 @@
+
+
+
+namespace Content.Server.OnLand
+{
+
+    [RegisterComponent]
+    public sealed partial class TriggerOnLandComponent : Component
+    {
+
+        [DataField]
+        public int Percent = 0;
+
+
+    }
+
+}
