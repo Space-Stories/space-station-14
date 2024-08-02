a338fd1e4c662fa097bbcf74d827f6e253f30b3d
Doublechest <twin01v@gmail.com>
2024-08-02 23:56:29 +0300

    some changes

---
+++ Content.Client/Stories/Preferences/Loadouts/Effects/PartnerLoadoutEffect.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics.CodeAnalysis;
+using Content.Client.Corvax.Sponsors;
+using Content.Shared.Humanoid.Prototypes;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Utility;
+
+namespace Content.Shared.Preferences.Loadouts.Effects;
+
+public sealed partial class PartnerLoadoutEffect : LoadoutEffect
+{
+    [DataField(required: true)]
+    public float Tier = 1f;
+
+    public override bool Validate(HumanoidCharacterProfile profile, RoleLoadout loadout, ICommonSession? session, IDependencyCollection collection,
+        [NotNullWhen(false)] out FormattedMessage? reason)
+    {
+        var manager = collection.Resolve<SponsorsManager>();
+        reason = FormattedMessage.FromUnformatted(Loc.GetString("loadout-group-partner-tier-restriction"));
+        if (!manager.TryGetInfo(out var sponsorInfo) || sponsorInfo.Tier < Tier)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
