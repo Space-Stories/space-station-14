8d693effabda1ec99a03adb887156c0964bf9049
Aristophan_ | Иван <taksak426@gmail.com>
2024-08-31 11:06:14 +0400

    Коммит неверного кода(

---
+++ Content.Shared/Stories/ChameleonStamp/ChameleonStampComponent.cs
@@ -0,0 +1,65 @@
+using Content.Shared.Stories.ChameleonStamp;
+using Content.Shared.Inventory;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Stories.ChameleonStamp;
+
+/// <summary>
+///     Allow players to change clothing sprite to any other clothing prototype.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+[Access(typeof(SharedChameleonStampSystem))]
+public sealed partial class ChameleonStampComponent : Component
+{
+    /// <summary>
+    ///     Filter possible chameleon options by their slot flag.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadOnly)]
+    [DataField(required: true)]
+    public SlotFlags Slot = SlotFlags.All;
+
+    /// <summary>
+    ///     EntityPrototype id that chameleon item is trying to mimic.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadOnly)]
+    [DataField(required: true), AutoNetworkedField]
+    public EntProtoId? Default;
+
+    /// <summary>
+    ///     Current user that wears chameleon clothing.
+    /// </summary>
+    [ViewVariables]
+    public EntityUid? User;
+}
+
+[Serializable, NetSerializable]
+public sealed class ChameleonStampBoundUserInterfaceState : BoundUserInterfaceState
+{
+    public readonly SlotFlags Slot;
+    public readonly string? SelectedId;
+
+    public ChameleonStampBoundUserInterfaceState(SlotFlags slot, string? selectedId)
+    {
+        Slot = slot;
+        SelectedId = selectedId;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class ChameleonStampPrototypeSelectedMessage : BoundUserInterfaceMessage
+{
+    public readonly string SelectedId;
+
+    public ChameleonStampPrototypeSelectedMessage(string selectedId)
+    {
+        SelectedId = selectedId;
+    }
+}
+
+[Serializable, NetSerializable]
+public enum ChameleonUiKey : byte
+{
+    Key
+}
