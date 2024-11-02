b0f6f05568bad9529cad218ab58e89ec03040daa
Theodore Lukin <66275205+pheenty@users.noreply.github.com>
2024-11-02 22:52:41 +0700

    Очки термального зрения (#138)
    * thermalvisionclothingcomponent
    
    * toggling
    
    * sprites, uplink listings, shadowling interaction
    
    * action icon
    
    * its silent anyways
    
    * unnecessary dependency
    
    * less hardcode
    
    * change meta
    
    * change licensing
    
    * unhardcode

--- Content.Shared/Stories/ThermalVision/ThermalVisionComponent.cs
+++ Content.Shared/Stories/ThermalVision/ThermalVisionComponent.cs
@@ -1,10 +1,18 @@
-﻿using Robust.Shared.GameStates;
+﻿using Content.Shared.Actions;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared.Stories.ThermalVision;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 public sealed partial class ThermalVisionComponent : Component
 {
+    [ViewVariables(VVAccess.ReadWrite), DataField("enabled"), AutoNetworkedField]
+    public bool Enabled { get; set; } = false;
+    [ViewVariables(VVAccess.ReadWrite), DataField("innate"), AutoNetworkedField]
+    public bool Innate { get; set; } = false;
+    [DataField]
+    public string ToggleAction = "ToggleThermalVisionAction";
     [DataField, AutoNetworkedField]
-    public bool Enabled = true;
+    public EntityUid? ToggleActionEntity;
 }
+public sealed partial class ToggleThermalVisionEvent : InstantActionEvent { }
